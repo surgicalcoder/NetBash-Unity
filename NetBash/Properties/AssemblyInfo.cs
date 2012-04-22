01a7343b654b7c4bceeb46e1e5e888f6ecc15155
Monty Alhakim <monty@dotnetpro.co.uk>
2012-04-22 11:38:45 +0100

    Assembly Info change.

--- NetBash/Properties/AssemblyInfo.cs
+++ NetBash/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetBash")]
+[assembly: AssemblyTitle("NetBash-Unity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetBash-DotNetPro")]
+[assembly: AssemblyProduct("NetBash-Unity-DotNetPro")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
